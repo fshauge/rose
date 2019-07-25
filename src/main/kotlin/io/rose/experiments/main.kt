000e19e3a0d2f0c899aaf42f7654155f51d3fb3d
Markus Hauge <markus.hauge@hotmail.com>
2019-07-25 23:16:59 +0200

    Extract JFrame from Container to increase modularity

--- src/main/kotlin/io/rose/experiments/main.kt
+++ src/main/kotlin/io/rose/experiments/main.kt
@@ -1,7 +1,13 @@
 package io.rose.experiments
 
 import io.rose.Container
+import java.awt.Dimension
+import javax.swing.JFrame
 
 fun main() {
-    Container(app()).run()
+    val frame = JFrame("App")
+    frame.add(Container(app()))
+    frame.size = Dimension(800, 600)
+    frame.setLocationRelativeTo(null)
+    frame.isVisible = true
 }
